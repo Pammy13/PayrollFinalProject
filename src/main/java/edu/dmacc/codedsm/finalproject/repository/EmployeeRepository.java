6ca2eac406a914abbf2d4e215e775fd1aaaa4f9b
Pammy Hunter <pjhunter@dmacc.com>
2019-05-17 12:40:20 -0500

    clean up

---
+++ src/main/java/edu/dmacc/codedsm/finalproject/repository/EmployeeRepository.java
@@ -0,0 +1,38 @@
+package edu.dmacc.codedsm.finalproject.repository;
+
+import edu.dmacc.codedsm.finalproject.model.Employee;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeRepository{
+
+    private HashMap<String, Employee> employees = new HashMap<>();
+
+    public HashMap<String, Employee> getEmployees() {return employees;
+    }
+
+    public void setEmployees(HashMap<String, Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Collection<Employee> getAllEmployees(){
+        return employees.values();
+    }
+
+    public Employee getEmployee(String id) {
+        if (employees.containsKey(id)) {
+            Employee employee = employees.get(id);
+            return employee;
+        } else {
+            return null;
+        }
+    }
+
+    public void saveEmployee(Employee employee) {
+        employees.put(employee.getIdNumber(), employee);
+            }
+
+
+}
---
+++ src/main/java/edu/dmacc/codedsm/finalproject/service/DataLoaderService.java
@@ -0,0 +1,55 @@
+package edu.dmacc.codedsm.finalproject.service;
+
+import edu.dmacc.codedsm.finalproject.model.Employee;
+import edu.dmacc.codedsm.finalproject.repository.EmployeeRepository;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataLoaderService {
+
+    private static final String FILE_NAME = "initial_load.txt";
+
+    public void loadEmployees(EmployeeRepository employeeRepository){
+        BufferedReader bufferReader = null;
+        try {
+            FileReader inputFile = new FileReader(FILE_NAME);
+            bufferReader = new BufferedReader(inputFile);
+            String line;
+
+            while ((line = bufferReader.readLine()) != null) {
+
+                Employee employee = createEmployee(line);
+                employeeRepository.saveEmployee(employee);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Employee createEmployee(String line) {
+        String idNumber = line.substring(0, line.indexOf(","));
+        String restOfString = line.substring(line.indexOf(",") + 1);
+        String name = restOfString.substring(0, restOfString.indexOf(","));
+        String wage = restOfString.substring(restOfString.indexOf(",") + 1);
+        Employee employee = new Employee();
+        employee.setIdNumber(idNumber);
+        employee.setName(name);
+        employee.setHourlyRate(Double.parseDouble(wage));
+        employee.setHoursWorked(0.0);
+        return employee;
+    }
+
+}
+
+
+
+
